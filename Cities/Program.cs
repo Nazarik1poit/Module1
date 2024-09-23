776dfdca93ba435e9f3e8ec643634a9c8a1a1794
Бурдыко Назар <burdykonazar@gmail.com>
2024-09-23 12:51:19 +0300

    Структура программы изменена чтобы она была цикличная

--- Cities/Program.cs
+++ Cities/Program.cs
@@ -1,16 +1,31 @@
-﻿while (true)
+﻿using System;
+
+class Program
 {
-    string[] towns = ["Рейкъявик", "Уфа", "Витебск", "Борисов", "Бразилиа"];
-    Console.WriteLine("Введите название города");
-    string input = Console.ReadLine();
-    for (int i = 0; i < towns.Length; i++)
+    static void Main()
     {
-        if (input == towns[i])
+        while (true)
         {
-            Console.WriteLine($"Вы угадали! {input} находиться на {i} позиции в списке.");
-            Console.ReadKey();
-            break;
+            string[] towns = { "Рейкъявик", "Уфа", "Витебск", "Борисов", "Бразилиа" };
+            Console.WriteLine("Введите название города:");
+            string input = Console.ReadLine();
+            bool found = false;  // Флаг, чтобы отслеживать, был ли найден город
+            for (int i = 0; i < towns.Length; i++)
+            {
+                if (input == towns[i])
+                {
+                    Console.WriteLine($"Вы угадали! {input} находится на {i}-й позиции в списке.");
+                    found = true;
+                    break;  // Выход из цикла, так как город найден
+                }
+            }
+            if (!found)
+            {
+                Console.WriteLine("Вы не угадали, попробуйте ещё раз.");
+            }
+            Console.WriteLine("Нажмите любую клавишу для продолжения");
+            Console.ReadKey(); 
+            Console.Clear();  
         }
     }
-    Console.WriteLine("Вы не угадали, попробуйте ещё раз");
 }
