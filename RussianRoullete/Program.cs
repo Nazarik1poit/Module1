d7788f1d8b16df41a2832fcc1e9420845790d8bc
Бурдыко Назар <burdykonazar@gmail.com>
2024-09-23 11:49:05 +0300

    Было добавлено 5 задание

---
+++ RussianRoullete/Program.cs
@@ -0,0 +1,13 @@
+﻿Random random = new Random();
+int randint = random.Next(1, 11);
+Console.WriteLine("Введите рандомное число");
+int input = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Было загадано " + randint);
+if (input == randint)
+{
+    Console.WriteLine("Вы угадали!");
+}
+else
+{
+    Console.WriteLine("Попробуйте ещё раз");
+}
