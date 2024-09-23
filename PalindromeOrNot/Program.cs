20fcad108b9cd7ff0fb3a2f3f877f353767bbe38
Бурдыко Назар <burdykonazar@gmail.com>
2024-09-23 12:26:10 +0300

    Работа программы занесена в бесконечный цикл

--- PalindromeOrNot/Program.cs
+++ PalindromeOrNot/Program.cs
@@ -1,22 +1,24 @@
 ﻿using System.Diagnostics;
-
-Console.WriteLine("Введите строку");
-string textIn = Console.ReadLine();
-textIn = textIn.ToLower().Replace(" ", "").Replace(",", "").Replace(".", "").Replace("!", "");
-int length = textIn.Length;
-bool isPalindrome = true;
-for (int i = 0; i < length / 2; i++)
+while (true)
 {
-    if (textIn[i] != textIn[length - i - 1])
+    Console.WriteLine("Введите строку");
+    string textIn = Console.ReadLine();
+    textIn = textIn.ToLower().Replace(" ", "").Replace(",", "").Replace(".", "").Replace("!", "");
+    int length = textIn.Length;
+    bool isPalindrome = true;
+    for (int i = 0; i < length / 2; i++)
     {
-        isPalindrome = false;
+        if (textIn[i] != textIn[length - i - 1])
+        {
+            isPalindrome = false;
+        }
     }
-}
-if (isPalindrome)
-{
-    Console.WriteLine("Палиндром");
-}
-else
-{
-    Console.WriteLine("Не палиндром");
+    if (isPalindrome)
+    {
+        Console.WriteLine("Палиндром");
+    }
+    else
+    {
+        Console.WriteLine("Не палиндром");
+    }
 }
