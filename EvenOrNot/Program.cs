b49cd36e0a89dd588f337b1897922d1939b917d7
Бурдыко Назар <burdykonazar@gmail.com>
2024-09-23 10:52:43 +0300

    Добавлено 2 задание

---
+++ EvenOrNot/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целое число:");
+int number = Convert.ToInt32(Console.ReadLine()); 
+if (number % 2 == 0)
+{
+    Console.WriteLine("Чётное!");
+}
+else
+{
+    {
+        Console.WriteLine("Нечётное!");
+    }
+}
