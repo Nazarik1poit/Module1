1597e97ebc984e41a317c8d25f4775165243fc7a
Бурдыко Назар <burdykonazar@gmail.com>
2024-09-23 12:19:13 +0300

    Работа программы занесена в бесконечный цикл

--- EvenOrNot/Program.cs
+++ EvenOrNot/Program.cs
@@ -1,14 +1,17 @@
-﻿Console.WriteLine("Введите целое число:");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number == 0)
+﻿while (true)
 {
-    Console.WriteLine("0 нельзя отнести к чётному или нечётному");
-}
-else if (number % 2 == 0)
-{
-    Console.WriteLine("Чётное!");
-}
-else
-{
-    Console.WriteLine("Нечётное!");
+    Console.WriteLine("Введите целое число:");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number == 0)
+    {
+        Console.WriteLine("0 нельзя отнести к чётному или нечётному");
+    }
+    else if (number % 2 == 0)
+    {
+        Console.WriteLine("Чётное!");
+    }
+    else
+    {
+        Console.WriteLine("Нечётное!");
+    }
 }
