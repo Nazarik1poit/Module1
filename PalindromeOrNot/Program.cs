cedd7311b4addea36130050dfe2e45e63e5c3e44
Бурдыко Назар <burdykonazar@gmail.com>
2024-09-23 11:16:46 +0300

    Добавлено 3 задание

---
+++ PalindromeOrNot/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("Введите строку");
+string textIn = Console.ReadLine();
+textIn = textIn.ToLower().Replace(" ", "").Replace(",", "").Replace(".", "").Replace("!", "");
+int length = textIn.Length;
+bool isPalindrome = true;
+for (int i = 0; i < length / 2; i++)
+{
+    if (textIn[i] != textIn[length - i - 1])
+    {
+        isPalindrome = false;
+    }
+}
+if (isPalindrome)
+{
+    Console.WriteLine("Палиндром");
+}
+else
+{
+    Console.WriteLine("Не палиндром");
+}
