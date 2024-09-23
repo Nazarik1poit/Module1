01dce33bc4d300765b68fd28a5d419d0f2c9ceb4
Бурдыко Назар <burdykonazar@gmail.com>
2024-09-23 12:53:32 +0300

    Программа зациклена

--- RussianRoullete/Program.cs
+++ RussianRoullete/Program.cs
@@ -1,13 +1,16 @@
-﻿Random random = new Random();
-int randint = random.Next(1, 11);
-Console.WriteLine("Введите рандомное число");
-int input = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Было загадано " + randint);
-if (input == randint)
+﻿while (true)
 {
-    Console.WriteLine("Вы угадали!");
-}
-else
-{
-    Console.WriteLine("Попробуйте ещё раз");
+    Random random = new Random();
+    int randint = random.Next(1, 11);
+    Console.WriteLine("Введите рандомное число");
+    int input = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Было загадано " + randint);
+    if (input == randint)
+    {
+        Console.WriteLine("Вы угадали!");
+    }
+    else
+    {
+        Console.WriteLine("Попробуйте ещё раз");
+    }
 }
