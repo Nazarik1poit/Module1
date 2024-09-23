8b397394588c71574517d1696e89822869e34ff8
Бурдыко Назар <burdykonazar@gmail.com>
2024-09-23 10:17:49 +0300

    Разработан и реализован интерфейс

--- FarenheitToCelcius/Form1.Designer.cs
+++ FarenheitToCelcius/Form1.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            domainUpDown1 = new DomainUpDown();
+            domainUpDown2 = new DomainUpDown();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(67, 36);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(67, 86);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(54, 119);
+            button1.Name = "button1";
+            button1.Size = new Size(151, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Конвертировать";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // domainUpDown1
+            // 
+            domainUpDown1.Items.Add("C");
+            domainUpDown1.Items.Add("F");
+            domainUpDown1.Location = new Point(21, 87);
+            domainUpDown1.Name = "domainUpDown1";
+            domainUpDown1.Size = new Size(40, 27);
+            domainUpDown1.TabIndex = 3;
+            domainUpDown1.Text = "F";
+            // 
+            // domainUpDown2
+            // 
+            domainUpDown2.Items.Add("F");
+            domainUpDown2.Items.Add("C");
+            domainUpDown2.Location = new Point(21, 37);
+            domainUpDown2.Name = "domainUpDown2";
+            domainUpDown2.Size = new Size(40, 27);
+            domainUpDown2.TabIndex = 4;
+            domainUpDown2.Text = "C";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(241, 302);
+            Controls.Add(domainUpDown2);
+            Controls.Add(domainUpDown1);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "FtoC";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private DomainUpDown domainUpDown1;
+        private DomainUpDown domainUpDown2;
     }
 }
--- FarenheitToCelcius/Form1.cs
+++ FarenheitToCelcius/Form1.cs
@@ -6,5 +6,34 @@ namespace FarenheitToCelcius
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double numberIn = Convert.ToDouble(textBox1.Text);
+                double numberOut = 0;
+                // текущие параметры конвертирования
+                string param1 = domainUpDown2.Text;
+                string param2 = domainUpDown1.Text;
+                if (param1 == "F" && param2 == "C")
+                {
+                    numberOut = numberIn - 273;
+                }
+                else if (param1 == "C" && param2 == "F")
+                {
+                    numberOut = numberIn + 273;
+                }
+                else
+                {
+                    numberOut = numberIn;
+                }
+                textBox2.Text = Convert.ToString(numberOut);
+            }
+            catch
+            {
+                MessageBox.Show("Некорректный ввод");
+            }
+        }
     }
 }
