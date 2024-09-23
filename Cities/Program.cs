48b3825b4ffddaca15daf3732aa7e534e9ce7048
Бурдыко Назар <burdykonazar@gmail.com>
2024-09-23 11:39:21 +0300

    Добавлено 4 задание

---
+++ Cities/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] towns = ["Рейкъявик", "Уфа", "Витебск", "Борисов", "Бразилиа"];
+Console.WriteLine("Введите название города");
+string input = Console.ReadLine();
+for (int i = 0; i < towns.Length; i++)
+{
+    if (input == towns[i])
+    {
+        Console.WriteLine($"Вы угадали! {input} находиться на {i} позиции в списке.");
+        Console.ReadKey();
+        Environment.Exit(0);
+    }
+}
+Console.WriteLine("Вы не угадали, попробуйте ещё раз");
--- EvenOrNot/Program.cs
+++ EvenOrNot/Program.cs
@@ -1,12 +1,14 @@
 ﻿Console.WriteLine("Введите целое число:");
-int number = Convert.ToInt32(Console.ReadLine()); 
-if (number % 2 == 0)
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 0)
+{
+    Console.WriteLine("0 нельзя отнести к чётному или нечётному");
+}
+else if (number % 2 == 0)
 {
     Console.WriteLine("Чётное!");
 }
 else
 {
-    {
-        Console.WriteLine("Нечётное!");
-    }
+    Console.WriteLine("Нечётное!");
 }
