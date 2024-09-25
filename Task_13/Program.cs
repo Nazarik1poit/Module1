e4b9e2497db13341083ba68d779a8dbe86fda715
Бурдыко Назар <burdykonazar@gmail.com>
2024-09-25 12:14:33 +0300

    13 задание

---
+++ Task_13/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите число: ");
+int maxsumm = int.Parse(Console.ReadLine());
+int[] array = new int[maxsumm*3];
+int i = 0;
+Random rnd = new Random();
+int sum = 0;
+while (true)
+{
+    int temp = rnd.Next(1, 10);
+    if (sum + temp < maxsumm)
+    {
+        array[i] = temp; 
+        sum += temp;
+        i++;
+    }
+    else {
+        break;
+    }
+}
+Console.WriteLine($"Сумма: {sum}");
+int[] optarray = new int[i];
+for (int j = 0; j < optarray.Length; j++)
+{
+    optarray[j] = array[j];
+    Console.WriteLine($"array[{j}] = {optarray[j]}");
+}
