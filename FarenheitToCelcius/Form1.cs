728914d3cde295f70cb048ccaf797227450a6d20
Бурдыко Назар <burdykonazar@gmail.com>
2024-09-25 11:30:56 +0300

    исправлена формула

--- FarenheitToCelcius/Form1.cs
+++ FarenheitToCelcius/Form1.cs
@@ -13,27 +13,33 @@ namespace FarenheitToCelcius
             {
                 double numberIn = Convert.ToDouble(textBox1.Text);
                 double numberOut = 0;
+
                 // текущие параметры конвертирования
-                string param1 = domainUpDown2.Text;
-                string param2 = domainUpDown1.Text;
+                string param1 = domainUpDown2.Text; // Исходная единица
+                string param2 = domainUpDown1.Text; // Целевая единица
+
+                // Конвертация из Фаренгейтов в Цельсии
                 if (param1 == "F" && param2 == "C")
                 {
-                    numberOut = numberIn - 273;
+                    numberOut = (numberIn - 32) * 5 / 9;
                 }
+                // Конвертация из Цельсиев в Фаренгейты
                 else if (param1 == "C" && param2 == "F")
                 {
-                    numberOut = numberIn + 273;
+                    numberOut = (numberIn * 9 / 5) + 32;
                 }
                 else
                 {
-                    numberOut = numberIn;
+                    numberOut = numberIn; // Если выбран одинаковый формат, просто копируем значение
                 }
+
                 textBox2.Text = Convert.ToString(numberOut);
             }
             catch
             {
                 MessageBox.Show("Некорректный ввод");
             }
+
         }
     }
 }
